9c31c60e9d0eb875dc0b9539cb213fcee3ee4762
Dryden Pienaar <pienaardryden@gmail.com>
2026-05-17 17:27:53 +0200

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Navbar } from '@/components/Navbar'
+import HeaderSlider from '@/components/HeaderSlider'
+
+export default function page() {
+  return (
+    <div>
+      <Navbar />
+      <HeaderSlider />
+    </div>
+  )
+}
+
